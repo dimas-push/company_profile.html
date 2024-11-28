b16d5df1f52e6e41d4ff90184e087e576940a384
DIMAS <if23.dimaslystianto@mhs.ubpkarawang.ac.id>
2024-11-28 12:54:27 +0700

    Add files via upload

--- pemweb-db.sql
+++ pemweb-db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 27, 2024 at 03:33 PM
+-- Generation Time: Nov 03, 2023 at 02:59 AM
 -- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `pemweb-db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `image`, `created_at`, `updated_at`) VALUES
+(39, 'sabun', 5000, '/upload/WhatsApp Image 2023-11-02 at 19.44.03.jpeg', '2023-11-02 13:30:25', NULL),
+(40, 'detergen', 5000, '/upload/1698932052-WhatsApp Image 2023-11-02 at 19.44.03.jpeg', '2023-11-02 13:34:12', NULL),
+(41, 'detergen', 5000, '/upload/1698932166-2b83cde3f290082bed3f41594bda2383-WhatsApp Image 2023-11-02 at 19.44.03.jpeg', '2023-11-02 13:36:06', NULL),
+(42, 'so klin', 10000, '/upload/1698932550-f83bb0e67d37431c69b06cdccc1a58ca-panduan wisuda 2023-Gladi Mahasiswa.pdf', '2023-11-02 13:42:30', NULL);
 
 -- --------------------------------------------------------
 
@@ -43,11 +68,17 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `role`, `password`, `photo`, `created_at`, `updated_at`) VALUES
-(4, 'Dimas Lystianto', 'dimaslystianto11@gmail.com', 'user', '$2y$10$Iiwtgvr.yzyrYPYX3WoW0.QXkFaApGj4kDmtWYrNuHzryMdE.Eq9O', NULL, '2024-11-27 08:30:46', NULL);
+(5, 'Yusuf Eka Wicaksana', 'ekayusuf.wicaksana@gmail.com', 'user', '$2y$10$rLNoxFGQoNddoiWy/ebYdOkjhU0ff14xHdXPc2yJIi2APAWvQ.Cra', NULL, '2023-10-26 13:45:09', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +92,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- register.php
+++ register.php
@@ -1,57 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
-    <!-- Link Boostrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-
 </head>
-
 <body>
-
-    <form action="./backend/register.php" method="POST"
-        class="container position-absolute top-50 start-50 translate-middle row">
-        <div class="">
-            <h2>Register</h2>
-            <div class="mb-3 col-3">
-                <label for="nama" class="form-label">Nama</label>
-                <input type="text" class="form-control" id="nama" placeholder="Masukan Nama Anda" name="name">
-            </div>
-        </div>
-        <div class="mb-3 row">
-            <div class="col-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
-            </div>
-        </div>
-        <div class="mb-3">
-            <div class="col-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" placeholder="Masukan Password Anda" name="password">
-            </div>
-        </div>
-        <div class="mb-3">
-            <div class="col-3">
-                <label for="confirm" class="form-label">Konfirmasi Password</label>
-                <input type="password" name="confirm" placeholder="Konfirmasi Password Anda" class="form-control">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-6">
-                <input type="submit" value="login" name="submit" class="btn btn-primary">
-            </div>
-        </div>
+    <h1>Register</h1>
+    <form action="./backend/register.php" method="post">
+        <input type="text" name="name" placeholder="masukkan nama anda">
+        <input type="email" name="email" placeholder="masukkan email anda">
+        <input type="password" name="password" placeholder="masukkan password anda">
+        <input type="password" name="confirm" placeholder="masukkan konfirmasi password anda">
+        <input type="submit" value="login" name="submit">
     </form>
 
-
 </body>
-
 </html>
